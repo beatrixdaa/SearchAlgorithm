c4020bb5c37f74c1655e3348deb4cb736b67c413
Beatrix Devanti <beatrix.devanti.ft21@mail.umy.ac.id>
2022-10-28 16:00:43 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements un the array
+        int n;
+        //get the number of elements to score in the array
+        int i;
+
+        
+    }
+
+}
